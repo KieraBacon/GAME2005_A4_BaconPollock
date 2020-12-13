921606990c1a20b534cb1430061ed9a96fc12dae
Sam <samcpollock@gmail.com>
2020-12-12 20:59:23 -0500

    Added Startscene & various other small things
    - Added a start scene to the build 
          - Has names and a button 
    - Added a pause functionality 
        - Press "p" to activate pause 
    - Added in-game HUD with various variables 
    - Added in-game timer display

---
+++ GAME2005_A4_BaconPollock/Assets/PauseManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    bool paused = false;
+    public GameObject pauseUI;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            paused = togglePause();
+            Debug.Log("PAUSE PRESSED");
+            //Cursor.visible = true;
+        }
+    }
+
+    void OnGUI()
+    {
+        if (paused)
+        {
+            GUILayout.Label("Game is paused!");
+            if (GUILayout.Button("Click me to unpause"))
+                paused = togglePause();
+            pauseUI.SetActive(true);
+        }
+        else
+        {
+            pauseUI.SetActive(false);
+
+        }
+    }
+
+    bool togglePause()
+    {
+        if (Time.timeScale == 0f)
+        {
+            Time.timeScale = 1f;
+            return (false);
+        }
+        else
+        {
+            Time.timeScale = 0f;
+            return (true);
+        }
+    }
+}
---
+++ GAME2005_A4_BaconPollock/Assets/_Scripts/GameTimer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    public Text timeDisplay;
+    public float currentTime;
+    public float endTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        float roundedTime = Mathf.Round(currentTime);
+        timeDisplay.text = roundedTime.ToString();
+    }
+    public bool IsExpired()
+    {
+        return currentTime >= endTime;
+    }
+}
